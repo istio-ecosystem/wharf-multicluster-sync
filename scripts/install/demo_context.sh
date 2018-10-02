1401c5f51ee2fc9500efb213f53c36fb9ebbcd28
Yossi Mesika <mesika@il.ibm.com>
2018-10-02 14:16:36 +0300

    Updated the scripts for supporting deploying to a single or pair of clusters

--- scripts/install/demo_context.sh
+++ scripts/install/demo_context.sh
@@ -2,33 +2,33 @@
 
 if [ -z "$3" ]
   then
-    echo "Syntax: source demo_context.sh ca-name cluster1-name cluster2-name"
-    return
+    echo "Syntax: source demo_context.sh <ca-ctx> <cluster1-ctx> <cluster2-ctx>"
+    echo "where ca-ctx, cluster1-ctx and cluster2-ctx are the Kubeconfig context names for the relevent clusters."
+    exit 1
 fi
  
-export MB="kubernetes-admin@kubernetes"
 export CLUSTER1_NAME=$2
 export CLUSTER2_NAME=$3
 export CLUSTER1=$2
 export CLUSTER2=$3
 export ROOTCA_NAME=$1
 
-echo Checking ROOTCA $ROOTCA_NAME
+echo Checking access to ROOTCA cluster with context $ROOTCA_NAME
 if ! kubectl --context $ROOTCA_NAME version --short=true ; then
    echo $ROOTCA_NAME not accessable
-   return
+   exit 1
 fi
 
-echo Checking CLUSTER1 $CLUSTER1
+echo Checking access to Cluster 1 with context $CLUSTER1
 if ! kubectl --context $CLUSTER1 version --short=true ; then
    echo $CLUSTER1 not accessable
-   return
+   exit 1
 fi
 
-echo Checking CLUSTER2 $CLUSTER2
+echo Checking access to Cluster 2 with context $CLUSTER2
 if ! kubectl --context $CLUSTER2 version --short=true ; then
    echo $CLUSTER2 not accessable
-   return
+   exit 1
 fi
 
 echo Success setting environment for three clusters
--- scripts/install/deploy_cluster.sh
+++ scripts/install/deploy_cluster.sh
@@ -3,17 +3,8 @@ set -o errexit
 
 AGENT_NS=default
 
-if [ -z "$2" ]
-  then
-    echo "Syntax: deploy_cluster.sh client-cluster peer-cluster [peer-cluster]*"
-    exit 1
-fi
-
-if [ -z "$3" ]
-  then
-    echo ""
-  else
-    echo "Unimplemented: Create config map for multiple services.  Use one peer for now."
+if ! [ -z "$3" ]; then
+    echo "Unimplemented: Create config map for multiple services. Use one peer for now."
     exit 1
 fi
 
@@ -29,14 +20,29 @@ CLIENT_CLUSTER=$1
 CLIENT_IP=`kubectl --context ${CLIENT_CLUSTER} get service istio-ingressgateway -n istio-system -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
 shift
 
+if [ "$#" -eq 0 ]; 
+then
+  PEERS="WatchedPeers: []"
+else
 # TODO Create SERVER_IP as list so we can create ConfigMap with list of peers
 for SERVER_CLUSTER in "$@"
 do
 	SERVER_IP=`kubectl --context ${SERVER_CLUSTER} get service istio-ingressgateway -n istio-system -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
 	SERVER_AGENT_IP=`kubectl --context ${SERVER_CLUSTER} get service mc-agent -n $AGENT_NS -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`
-	SERVER_AGENT_PORT=`kubectl --context ${SERVER_CLUSTER} get service mc-agent -n $AGENT_NS -o jsonpath='{.spec.ports[0].nodePort}'`
 	echo $CLIENT_CLUSTER is a client of $SERVER_CLUSTER with Ingress Gateway at $SERVER_IP
 done
+
+  PEERS=$(cat <<-END
+WatchedPeers:
+      - ID: $SERVER_CLUSTER
+        GatewayIP: $SERVER_IP
+        GatewayPort: 80
+        AgentIP: $SERVER_AGENT_IP
+        AgentPort: 80
+        ConnectionMode: $CONNECTION_MODE
+END
+)
+fi
 
 # Create ConfigMap to configure agent
 set +e
@@ -54,17 +60,11 @@ data:
       GatewayIP: $CLIENT_IP
       GatewayPort: 80
       AgentPort: 8999
-      TrustedPeers: []
-      WatchedPeers:
-      - ID: $SERVER_CLUSTER
-        GatewayIP: $SERVER_IP
-        GatewayPort: 80
-        AgentIP: $SERVER_AGENT_IP
-        AgentPort: $SERVER_AGENT_PORT
-        ConnectionMode: $CONNECTION_MODE
+      TrustedPeers:
+      - "*"
+      $PEERS
 EOF
 set -e
 	
-# Create Pod
+# Deploy the MC agent service
 kubectl --context $CLIENT_CLUSTER apply -f deploy.yaml
-kubectl --context $CLIENT_CLUSTER apply -f mc_gateways.yaml
