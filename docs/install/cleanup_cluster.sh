6ce2e0d500bd2f738779323eaa1197be3562806c
banix <MBanikazemi@gmail.com>
2018-10-10 17:08:17 -0400

    Cleanup for new ingressgateway service port

--- docs/install/cleanup_cluster.sh
+++ docs/install/cleanup_cluster.sh
@@ -8,3 +8,4 @@ fi
 
 kubectl delete --context=$1 -f deploy.yaml
 kubectl delete --context=$1 -n istio-system configmaps mc-configuration
+kubectl --context=$1 -n istio-system patch service istio-ingressgateway --type=json --patch='[{"op": "test", "path": "/spec/ports/0/port", "value": 31444}, {"op": "remove", "path": "/spec/ports/0"}]' || true
