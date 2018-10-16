9109b389732b38db09c966d56f44c70bac9bdb05
Yossi Mesika <mesika@il.ibm.com>
2018-10-16 11:13:24 +0300

    Correctly handle cluster names which aren't lower case

--- docs/install/install_citadel.sh
+++ docs/install/install_citadel.sh
@@ -10,9 +10,8 @@ B64_DECODE=${BASE64_DECODE:-base64 --decode}
 
 for CLUSTER in ${CLUSTER1} ${CLUSTER2} ${CLUSTER3}
 do
-  kubectl --context ${ROOTCA} -n istio-system create serviceaccount istio-citadel-service-account-${CLUSTER} || true
-
-  SERVICE_ACCOUNT="istio-citadel-service-account-${CLUSTER}"
+  SERVICE_ACCOUNT=$(echo "istio-citadel-service-account-$CLUSTER" | tr '[:upper:]' '[:lower:]') #lower case to make it valid service account name
+  kubectl --context ${ROOTCA} -n istio-system create serviceaccount ${SERVICE_ACCOUNT} || true
   CERT_NAME="istio.${SERVICE_ACCOUNT}"
   DIR="/tmp/ca/${CLUSTER}"
   mkdir -p $DIR
@@ -30,10 +29,10 @@ do
 
   kubectl --context ${CLUSTER} create secret generic cacerts -n istio-system \
           --from-file=${DIR}/ca-cert.pem --from-file=${DIR}/ca-key.pem \
-          --from-file=${DIR}/root-cert.pem --from-file=${DIR}/cert-chain.pem
+          --from-file=${DIR}/root-cert.pem --from-file=${DIR}/cert-chain.pem || true
 
 
-  kubectl --context ${CLUSTER} delete  deployment  -n istio-system  istio-citadel
-  sed -e "s/__CLUSTERNAME__/${CLUSTER}/g;s/__ROOTCA_HOST__/${rootca_host}/g" istio-citadel-new.yaml | kubectl --context ${CLUSTER} apply -f -
-  kubectl --context ${CLUSTER} apply -f  istio-auto-injection.yaml || true
+  kubectl --context ${CLUSTER} delete deployment -n istio-system istio-citadel || true
+  sed -e "s/__SERVICE_ACCOUNT__/${SERVICE_ACCOUNT}/g;s/__ROOTCA_HOST__/${rootca_host}/g" istio-citadel.yaml | kubectl --context ${CLUSTER} apply -f -
+  kubectl --context ${CLUSTER} apply -f istio-auto-injection.yaml || true
 done
